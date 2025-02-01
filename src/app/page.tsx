021699018a50372ed971cd5019369b581599de81
dangchinh25 <dangchinh2501@gmail.com>
2025-02-01 13:52:32 -0800

    example column filter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 'use client'
 
-import { createColumnHelper, flexRender, getCoreRowModel, useReactTable } from "@tanstack/react-table";
+import { createColumnHelper, flexRender, getCoreRowModel, useReactTable, ColumnFilter, ColumnFiltersState, getSortedRowModel, getPaginationRowModel, getFilteredRowModel, Column } from "@tanstack/react-table";
 import { trpcClient } from "./_trpc/client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 
 type Person = {
@@ -58,29 +58,42 @@ const columns = [
     header: () => 'Age',
     cell: info => info.renderValue(),
     footer: info => info.column.id,
+    enableColumnFilter: false
   }),
   columnHelper.accessor('visits', {
     header: () => <span>Visits</span>,
     footer: info => info.column.id,
+    enableColumnFilter: false
   }),
   columnHelper.accessor('status', {
     header: 'Status',
     footer: info => info.column.id,
+    enableColumnFilter: false
   }),
   columnHelper.accessor('progress', {
     header: 'Profile Progress',
     footer: info => info.column.id,
+    enableColumnFilter: false
   }),
 ]
 
 export default function Home() {
   const [data, setData] = useState(() => [...defaultData]);
-
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>(
+    []
+  )
 
   const table = useReactTable({
     data: data,
     columns,
     getCoreRowModel: getCoreRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    onColumnFiltersChange: setColumnFilters,
+    state: {
+      columnFilters,
+    },
   })
 
   return (
@@ -97,6 +110,11 @@ export default function Home() {
                         header.column.columnDef.header,
                         header.getContext()
                       )}
+                      {header.column.getCanFilter() ? (
+                        <div>
+                          <Filter column={header.column} />
+                        </div>
+                      ) : null}
                 </th>
               ))}
             </tr>
@@ -117,4 +135,86 @@ export default function Home() {
       <div className="h-4" />
     </div>
   );
+}
+
+function Filter({ column }: { column: Column<any, unknown> }) {
+  const columnFilterValue = column.getFilterValue()
+  const { filterVariant } = column.columnDef.meta ?? {}
+
+  return filterVariant === 'range' ? (
+    <div>
+      <div className="flex space-x-2">
+        {/* See faceted column filters example for min max values functionality */}
+        <DebouncedInput
+          type="number"
+          value={(columnFilterValue as [number, number])?.[0] ?? ''}
+          onChange={value =>
+            column.setFilterValue((old: [number, number]) => [value, old?.[1]])
+          }
+          placeholder={`Min`}
+          className="w-24 border shadow rounded"
+        />
+        <DebouncedInput
+          type="number"
+          value={(columnFilterValue as [number, number])?.[1] ?? ''}
+          onChange={value =>
+            column.setFilterValue((old: [number, number]) => [old?.[0], value])
+          }
+          placeholder={`Max`}
+          className="w-24 border shadow rounded"
+        />
+      </div>
+      <div className="h-1" />
+    </div>
+  ) : filterVariant === 'select' ? (
+    <select
+      onChange={e => column.setFilterValue(e.target.value)}
+      value={columnFilterValue?.toString()}
+    >
+      {/* See faceted column filters example for dynamic select options */}
+      <option value="">All</option>
+      <option value="complicated">complicated</option>
+      <option value="relationship">relationship</option>
+      <option value="single">single</option>
+    </select>
+  ) : (
+    <DebouncedInput
+      className="w-36 border shadow rounded"
+      onChange={value => column.setFilterValue(value)}
+      placeholder={`Search...`}
+      type="text"
+      value={(columnFilterValue ?? '') as string}
+    />
+    // See faceted column filters example for datalist search suggestions
+  )
+}
+
+// A typical debounced input react component
+function DebouncedInput({
+  value: initialValue,
+  onChange,
+  debounce = 500,
+  ...props
+}: {
+  value: string | number
+  onChange: (value: string | number) => void
+  debounce?: number
+} & Omit<React.InputHTMLAttributes<HTMLInputElement>, 'onChange'>) {
+  const [value, setValue] = useState(initialValue)
+
+  useEffect(() => {
+    setValue(initialValue)
+  }, [initialValue])
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      onChange(value)
+    }, debounce)
+
+    return () => clearTimeout(timeout)
+  }, [value])
+
+  return (
+    <input {...props} value={value} onChange={e => setValue(e.target.value)} />
+  )
 }
