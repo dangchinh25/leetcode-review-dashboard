90c61549f08a7d9633ddd36195dfce59b2d28475
dangchinh25 <dangchinh2501@gmail.com>
2025-02-01 00:22:49 -0800

    init server env

---
+++ src/shared/serverEnv.ts
@@ -0,0 +1,17 @@
+import {z} from 'zod'
+
+const envSchema = z.object({
+    POSTGRES_USER: z.string(),
+    POSTGRES_PASSWORD: z.string(),
+    POSTGRES_DB: z.string(),
+    POSTGRES_PORT: z.string(),
+    POSTGRES_HOST: z.string(),
+})
+
+const parsedServerEnv = envSchema.safeParse(process.env)
+
+if (!parsedServerEnv.success) {
+    throw new Error(`Invalid or missing environment variables:  ${parsedServerEnv.error.errors.join('\n')}`)
+}
+
+export const serverEnv = parsedServerEnv.data
